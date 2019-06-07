70cd3f6fc7d59c95fe0d0cc9beceda37c1e6081c
Andrea <om1noa@gmail.com>
2019-06-07 20:41:07 +0200

    Update tooltip.js to add null checks

--- assets/javascripts/bootstrap/tooltip.js
+++ assets/javascripts/bootstrap/tooltip.js
@@ -377,7 +377,9 @@
 
       var complete = function () {
         var prevHoverState = that.hoverState
-        that.$element.trigger('shown.bs.' + that.type)
+        if(that.$element){
+          that.$element.trigger('shown.bs.' + that.type)
+        }
         that.hoverState = null
 
         if (prevHoverState == 'out') that.leave(that)
@@ -630,7 +632,9 @@
     var that = this
     clearTimeout(this.timeout)
     this.hide(function () {
-      that.$element.off('.' + that.type).removeData('bs.' + that.type)
+      if(that.$element){
+        that.$element.off('.' + that.type).removeData('bs.' + that.type)
+      }
       if (that.$tip) {
         that.$tip.detach()
       }
